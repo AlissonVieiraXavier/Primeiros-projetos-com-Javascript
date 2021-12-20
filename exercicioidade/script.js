abd76519ed64e0ede3c73a9d7f9a99e43f7a71f3
Alisson Xavier <alissonvieiraxavier@hotmail.com>
2021-12-20 20:59:04 -0300

    h

--- exercicioidade/script.js
+++ exercicioidade/script.js
@@ -9,6 +9,54 @@ function verificar() {
     } else {
         let fsex = document.getElementsByName("radsex");
         let idade = ano - Number(fano.value);
+        let genero = "";
+        let img = document.createElement("img");
+        img.setAttribute("id", "foto");
+
+        if (fsex[0].checked) {
+            genero = "homem";
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute("src", "imgs/babyboy.jpg")
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute("src", "imgs/man.jpg")
+            }
+            else {
+                //idoso
+                img.setAttribute("src", "imgs/oldman.jpg")
+            }
+
+
+        } else if (fsex[1].checked) {
+            genero = "Mulher";
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute("src", "imgs/babygirl.jpg")
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute("src", "imgs/woman.jpg")
+            }
+            else {
+                //idoso
+                img.setAttribute("src", "imgs/oldwoman.jpg")
+            }
+
+        }
+        res.style.textAlign = "center";
+        res.innerHTML = ` Detectamos ${genero} com ${idade} anos`;
+
+        //adiciona a imagem
+        res.style.marginTop = "10px"
+        img.style.width = "500px";
+        img.style.width = "500px";
+        img.style.borderRadius = "100%";
+
+        res.appendChild(img);
+
+
+
+
 
 
     }
