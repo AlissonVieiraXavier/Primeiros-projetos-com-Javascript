e2cd7153e138b375aef274ba382b5c90718bcc11
Alisson Xavier <alissonvieiraxavier@hotmail.com>
2021-12-24 11:49:59 -0300

    dv

---
+++ exercicios repetição/script.js
@@ -0,0 +1,12 @@
+function contar() {
+    let inicio = document.getElementById("txt1")
+    let fim = document.querySelector("#txtf")
+    let passo = document.querySelector("#txtp")
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert("[ERRO] Faltam dados!")
+
+    } else {
+        alert("agaragabicth")
+    }
+}
