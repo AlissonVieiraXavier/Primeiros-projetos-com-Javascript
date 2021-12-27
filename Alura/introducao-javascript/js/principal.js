3dd79a8938501873babbc07a9eb0aaf4d6f0b9a3
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2021-12-27 19:19:21 -0300

    t

--- Alura/introducao-javascript/js/principal.js
+++ Alura/introducao-javascript/js/principal.js
@@ -1,2 +1,21 @@
-var titulo = document.querySelector("h1");
-titulo.textContent = "nhueeee";
+
+var paciente = document.querySelector("#primeiropaciente");
+
+var tdpeso = document.querySelector(".info-peso");
+peso = tdpeso.textContent;
+
+var tdaltura = document.querySelector(".info-altura");
+altura = tdaltura.textContent;
+
+
+
+
+
+
+
+
+
+
+
+console.log(peso);
+console.log(altura);
