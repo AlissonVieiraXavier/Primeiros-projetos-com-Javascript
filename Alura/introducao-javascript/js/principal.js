e07e06984946345ecb7053d8e4e01dd7074e516d
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2021-12-27 20:11:16 -0300

    g

--- Alura/introducao-javascript/js/principal.js
+++ Alura/introducao-javascript/js/principal.js
@@ -1,4 +1,5 @@
 
+// armazenando Valores
 var paciente = document.querySelector("#primeiropaciente");
 
 var tdpeso = document.querySelector(".info-peso");
@@ -7,6 +8,23 @@ peso = tdpeso.textContent;
 var tdaltura = document.querySelector(".info-altura");
 altura = tdaltura.textContent;
 
+var tdinfo = document.querySelector(".info-imc");
+info = tdinfo.textContent;
+
+
+if (peso < 0 || peso > 1000 || altura > 2.50) {
+    Alert(" Peso ou altura invalidos!");
+} else {
+    //calculo imc
+    var resultadoimc = peso / (altura * altura);
+
+    // apresentando resultado ao usuario
+    tdinfo.innerHTML = resultadoimc;
+
+    //segundo paciente
+
+
+}
 
 
 
@@ -17,5 +35,5 @@ altura = tdaltura.textContent;
 
 
 
-console.log(peso);
-console.log(altura);
+
+
