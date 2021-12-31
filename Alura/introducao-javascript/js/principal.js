00f0efa9a793a53c630b945cdaa9361eac8a00d0
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2021-12-31 12:10:28 -0300

    s

--- Alura/introducao-javascript/js/principal.js
+++ Alura/introducao-javascript/js/principal.js
@@ -46,18 +46,40 @@ botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
     var form = document.querySelector("#form-adiciona");
 
+    //armazena os valores
     var nome = form.nome.value;
     var peso = form.peso.value;
     var altura = form.altura.value;
     var gordura = form.gordura.value;
 
+    //createElemento(), cria o elemento tr e td
+
     var pacienteTr = document.createElement("tr");
 
     var nomeTd = document.createElement("td");
+    console.log(nomeTd)
     var pesoTd = document.createElement("td");
     var alturaTd = document.createElement("td");
     var gorduraTd = document.createElement("td");
     var imcTd = document.createElement("td");
+
+    // o conteudo de cada variavel recebe o valor digicado pelo usuario e ja coloca no elemento
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    // colocando um elemento dentro do outro com appendClild()
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    // adicionando os elementos na tabela
+    var adicionaElementosNaTabela = document.querySelector("#tabela-pacientes");
+    console.log(adicionaElementosNaTabela)
+    adicionaElementosNaTabela.appendChild(pacienteTr);
+
 
 
 
