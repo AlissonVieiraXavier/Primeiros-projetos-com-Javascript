23504d523e1921b36e35a53a4a7117f7ca468c60
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-01-08 14:21:07 -0300

    a

--- Alura/introducao-javascript/js/remover.js
+++ Alura/introducao-javascript/js/remover.js
@@ -1,8 +1,18 @@
 var pacientes = document.querySelectorAll(".paciente")
+var tabela = document.querySelector(".table");
 
-pacientes.forEach(function (paciente) {
+tabela.forEach("dblclick", function (event) {
+    console.log(event.target)
+
+})
+
+
+
+
+
+/*pacientes.forEach(function (paciente) {
     paciente.addEventListener("dblclick", function () {
         this.remove();
 
     })
-})
+})*/
