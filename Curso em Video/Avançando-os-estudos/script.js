154b163b67d39ea5d991ea99bdca09dfb257c261
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-01-09 16:32:38 -0300

    a

--- Curso em Video/Avançando-os-estudos/script.js
+++ Curso em Video/Avançando-os-estudos/script.js
@@ -1,12 +1,24 @@
 var lista = document.querySelector("#flista");
 var valor = document.querySelector(".input");
 var resultado = document.querySelector("#res");
+var valores = [];
 
-
-
-function adicionar() {
-    let armazenaValor = valor.value;
-    console.log(armazenaValor)
+//validação
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function inlista(n, l) {
 
 }
 
+function adicionar() {
+    if (isNumero(valor.value) && !inlista(num.value, valores)) {
+
+    } else {
+        alert("Valor inválido ou já adicionado!")
+    }
+}
