ace6fcf2bcb1ee4d61b7311bd7fac7caea75b7d7
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-01-10 17:39:04 -0300

    a

--- Curso em Video/Avançando-os-estudos/script.js
+++ Curso em Video/Avançando-os-estudos/script.js
@@ -45,21 +45,26 @@ function finalizar() {
          let soma = somar(valores);*/
         let maior = valores[0];
         let menor = valores[0];
+        let soma = 0;
+        let media = 0;
+
         for (let pos in valores) {
+            soma += valores[pos]
             if (valores[pos] > maior) {
                 maior = valores[pos];
             } if (valores[pos] < menor) {
                 menor = valores[pos];
             }
         }
-
+        media = soma / tot;
 
 
         resultado.innerHTML = "";
         resultado.innerHTML += `<p>Ao todo temos ${tot} numeros adicionados.</p>`
         resultado.innerHTML += `<p>O maior valor é o ${maior}.</p> `
         resultado.innerHTML += `<p>O menor valor é o ${menor}.</p> `
-        // resultado.innerHTML += `a soma dos valores adicionados ´é ${soma}.</p> `
+        resultado.innerHTML += `a soma dos valores adicionados é ${soma}.</p> `
+        resultado.innerHTML += `a media dos valores adicionados é ${media}.</p> `
     }
 
 }
