f9f465b604ccbd56d251221234aead12ac7c9f9f
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-01-12 16:58:04 -0300

    a

--- Alura/introducao-javascript/js/filtro.js
+++ Alura/introducao-javascript/js/filtro.js
@@ -1,4 +1,11 @@
 const campoFiltro = document.querySelector("#filtrar-tabela");
 campoFiltro.addEventListener('input', () => {
-    console.log("digitando...")
+
+    let pacientes = document.querySelectorAll(".paciente");
+    for (var i = 0; i < pacientes.length; i++) {
+        let paciente = pacientes[i];
+        let tdNome = document.querySelectorAll(".info-nome");
+        let nome = paciente.textContent;
+    }
+
 })
