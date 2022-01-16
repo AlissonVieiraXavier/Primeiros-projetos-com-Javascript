a16723843aba0acd7a35dfc624bd09c668ddcde8
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-01-16 10:48:11 -0300

    m

---
+++ Tutoriais-youtube/embaralhar-strings.js
@@ -0,0 +1,12 @@
+function makeid(characters) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
+let array = 'javascript';
+console.log(makeid(array));
