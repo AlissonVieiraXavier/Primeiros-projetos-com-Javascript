e908703038030968fd8faf089b880ea470f30fcb
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-01-16 11:33:56 -0300

    a

--- Tutoriais-youtube/embaralhar-strings.js
+++ Tutoriais-youtube/embaralhar-strings.js
@@ -1,8 +1,16 @@
-var word = 'javascript';
-var wordol = word.split("");
-var wordul = [];
+function sortWord(word) {
+    var wordol = word.split("");
+    var wordul = [];
 
-while (wordol.length) {
-    let sorteado = parseInt(Math.random * wordol.length);
-    let selectLetter = word.splice(sorteado, 1)[0];
+    while (wordol.length) {
+        let sorteado = parseInt(Math.random() * wordol.length);
+        let selectLetter = wordol.splice(sorteado, 1)[0];
+        wordul.push(selectLetter);
+    }
+    return wordul.join("");
 }
+
+
+//exemplo para teste;
+
+console.log(sortWord("Alisson"));
