e5f3c8ca10af12a2dfceecb74917d71ae5dfb1c2
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-01-19 18:00:01 -0300

    a

--- Alura/introducao-javascript/js/buscar-pacientes.js
+++ Alura/introducao-javascript/js/buscar-pacientes.js
@@ -1,11 +1,11 @@
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
 
-botaoAdicionar.addEventListener("click", function() {
+botaoAdicionar.addEventListener("click", function () {
     var xhr = new XMLHttpRequest();
 
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
-    xhr.addEventListener("load", function() {
+    xhr.addEventListener("load", function () {
         var erroAjax = document.querySelector("#erro-ajax");
 
         if (xhr.status == 200) {
@@ -13,7 +13,7 @@ botaoAdicionar.addEventListener("click", function() {
             var resposta = xhr.responseText;
             var pacientes = JSON.parse(resposta);
 
-            pacientes.forEach(function(paciente) {
+            pacientes.forEach(function (paciente) {
                 adicionaPacienteNaTabela(paciente);
             });
         } else {
