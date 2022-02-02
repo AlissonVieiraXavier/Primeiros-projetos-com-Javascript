1e8eb49c0cc78acd4416e9aa6b77ccd7044942de
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-02-02 17:31:31 -0300

    a

---
+++ Alura/Orientação-á-objetos/index.js
@@ -0,0 +1,10 @@
+const cliente1Nome = "Ricardo";
+const cliente1CPF = 11122233344;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+
+const cliente1Nome = "Alice";
+const cliente1CPF = 11629283844;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
