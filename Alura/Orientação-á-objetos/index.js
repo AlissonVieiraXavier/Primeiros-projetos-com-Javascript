e3fe0e53d968bc58c2246bd1f2cb414bf02f4f7f
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-02-03 17:15:24 -0300

    a

--- Alura/Orientação-á-objetos/index.js
+++ Alura/Orientação-á-objetos/index.js
@@ -17,6 +17,13 @@ const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 11629283844;
 
+const ContaCorrentRicardo = new ContaCorrente();
+ContaCorrentRicardo.agencia = 0010;
+ContaCorrentRicardo.saldo = 0;
+
+const ContaCorrentAlice = new ContaCorrente();
+ContaCorrentAlice.agencia = 0010;
+ContaCorrentAlice.saldo = 0;
 
 console.log(cliente1);
 console.log(cliente2);
