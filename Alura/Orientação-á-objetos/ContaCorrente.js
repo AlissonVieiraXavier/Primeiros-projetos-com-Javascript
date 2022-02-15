f35e58f0d3bdc432400e03c0533627f8a767f19a
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-02-15 17:37:18 -0300

    a

--- Alura/Orientação-á-objetos/ContaCorrente.js
+++ Alura/Orientação-á-objetos/ContaCorrente.js
@@ -1,32 +1,36 @@
 
-export class ContaCorrente{
+export class ContaCorrente {
     agencia;
-    cliente;
+    _cliente;
 
+    set cliente(novoValor) {
+        this._cliente = novoValor;
+    }
 
-     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+    // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
     _saldo = 0;
-    
 
-    sacar(valor){
-        if(this._saldo >= valor){
+
+
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
             this._saldo -= valor;
             return valor;
         }
     }
 
-    depositar(valor){
-        if(valor <= 0)
-        {
+    depositar(valor) {
+        if (valor <= 0) {
             return;
-        } 
-        this._saldo += valor;           
+        }
+        this._saldo += valor;
     }
 
-    tranferir(valor, conta){
-        
+    tranferir(valor, conta) {
+
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-        
+
     }
 }
--- Alura/Orientação-á-objetos/index.js
+++ Alura/Orientação-á-objetos/index.js
@@ -5,9 +5,9 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-//const cliente2 = new Cliente();
-//cliente2.nome = "Alice";
-//cliente2.cpf = 88822233309;
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
 
 
 const contaCorrenteRicardo = new ContaCorrente();
@@ -16,16 +16,13 @@ contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = new Cliente();
-conta2.cliente.nome = "Alice";
-conta2.cliente.cpf = "88822233309"
+conta2.cliente = 0;
 conta2.agencia = 102;
+console.log(conta2);
 
 let valor = 200;
 contaCorrenteRicardo.tranferir(valor, conta2);
 
-console.log("valor: ", valor)
-console.log(conta2);
-console.log(contaCorrenteRicardo);
+
 
 
