5aac52f04a9257aeb853a72cc0a61f5662ae442b
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-02-15 19:03:57 -0300

    a

--- Alura/Orientação-á-objetos/index.js
+++ Alura/Orientação-á-objetos/index.js
@@ -4,17 +4,15 @@ import { ContaCorrente } from "./ContaCorrente.js"
 const cliente1 = new Cliente("Ricardo", 11122233309);
 const cliente2 = new Cliente("Alice", 88822233309);
 
-console.log(cliente1.cpf);
+
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+console.log(contaCorrenteRicardo);
 
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
+const conta2 = new ContaCorrente(102, cliente2);
+console.log(conta2);
 
 let valor = 200;
 contaCorrenteRicardo.tranferir(valor, conta2);
