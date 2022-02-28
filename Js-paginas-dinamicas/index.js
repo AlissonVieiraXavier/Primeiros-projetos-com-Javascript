79fe5a28fed3164a5aaa37dfc2730d879f38e7d2
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-02-28 18:06:37 -0300

    a

---
+++ Js-paginas-dinamicas/index.js
@@ -0,0 +1,27 @@
+let teclaPom = document.querySelector(".tecla_pom").onclick = tocarTecla();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let teclaPomSom = document.querySelector("#som_tecla_pom");
+
+teclaPom.addEventListener('click', function () {
+    teclaPomSom.play();
+    console.log('alright')
+}
+);*/
+
