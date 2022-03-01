9880d990843f2f5fcd999b15f2df964da2b44b30
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-03-01 15:34:14 -0300

    a

--- Js-paginas-dinamicas/index.js
+++ Js-paginas-dinamicas/index.js
@@ -1,5 +1,25 @@
-function tocaSom() {
-    document.querySelector('#som_tecla_pom').play();
-}
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+};
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const IdAudio = `#som_${instrumento}`;
+
+
+
+    tecla.onclick = function () {
+        tocaSom(IdAudio);
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+    console.log(instrumento);
+}
