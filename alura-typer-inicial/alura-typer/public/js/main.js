3a28fe08bdedf46669b5a23e03e4f0be0604552c
AlissonVieiraXavier <alissonvieiraxavier@hotmail.com>
2022-04-10 15:16:52 -0300

    a

--- alura-typer-inicial/alura-typer/public/js/main.js
+++ alura-typer-inicial/alura-typer/public/js/main.js
@@ -1,10 +1,14 @@
 let frase = $(".frase").text();
-let numPalavras = frase.split(" ").length;
+let numPalavras = frase.split(/\S+/).length;
 let tamanhoDaFrase = $("#tamanhoDaFrase");
 tamanhoDaFrase.text(numPalavras);
 
 let campo = $(".campo-de-digitacao");
-campo.on("click", function(){
-    let numPalavrass = frase.split(" ").length;
-    campo.text(numPalavrass)
+campo.on("input", function(){
+  let conteudo = campo.val()
+  let quantidadePalavras = conteudo.split(/\S+/).length - 1;
+  $("#contador-palavras").text(quantidadePalavras);
+
+  let quantidadeCaracteres = conteudo.length;
+  $("#contador-caracteres").text(quantidadeCaracteres);
 })
